df70f33b419c6d60afeec949586b79bb93e16797
Thea Arias <99890039+tna113@users.noreply.github.com>
2022-03-16 14:53:28 -0400

    Add files via upload

---
+++ rubric/index.php
@@ -0,0 +1,17 @@
+<?php
+	$page='home';
+	$path='./';
+	require ($path.'assets/inc/header.php');
+?>
+
+<body>
+    <div class="about container">
+        <div class="textbox rubric">
+            <h2>RUBRIC</h2>
+            <p>I've modified my website in order to make it modular. There are separate headera nd footer files alongside different directories for increased speeds (through the use of cache) and organizational purposes. I've also added a comments.php file that connects to a database featuring different comments and messages from visitors. That page can be found on the footer of my website or you can click<a href="http://solace.ist.rit.edu/~ta7384/240/final/comments.php">HERE</a> for a direct link.</p><br>
+            <p>Unfortunately, I didn't have enough time to make my website mobile and tablet friendly. The same goes with my DHTML contribution though an easy way to fulfill this criteria would be to implement a fading transition within my page's image galleries. I'll be sure to do these in the near future.</p>
+        </div>
+    </div>
+</body>
+
+</html>
